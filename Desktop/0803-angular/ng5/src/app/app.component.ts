a83c80668648b1671cbd21d49978b568366862cf
miradork <katherine-kit.mirador@hpe.com>
2018-08-06 19:09:25 +0800

    first commit

---
+++ Desktop/0803-angular/ng5/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+//from library
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html', //HTML
+  styleUrls: ['./app.component.scss'] //CSS
+})
+//properties that define structure of component
+
+export class AppComponent {
+  title = 'ng5';
+}
+//logic
